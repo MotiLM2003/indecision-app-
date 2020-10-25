862e1c4ecf8660440fe5632b83499b7e9d018af9
Moti Elmakies <motiphone2003@gmail.com>
2020-10-25 17:54:58 +0200

    progress react tutorial

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,83 @@
 'use strict';
 
 console.log('App js is running 👍');
-console.log(react);
+
+var app = {
+  title: 'Title',
+  subTitle: 'sub title',
+  options: ['One', 'Two', 'Three', 'Four']
+};
+
+var isTitleExists = function isTitleExists() {
+  return app.title;
+};
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'this1 is 1JSX from app.js'
+  app.title && React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subTitle && React.createElement(
+    'p',
+    null,
+    'app.subTitle'
+  ),
+  app.options && app.options.length > 0 && React.createElement(
+    'p',
+    null,
+    'here are your options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    app.options.map(function (option, index) {
+      return React.createElement(
+        'li',
+        { key: index },
+        option
+      );
+    })
+  )
 );
-reactDOM.render(template, document.getElementById('root'));
+
+var count = 0;
+var addOne = function addOne() {
+  return console.log('AddOne');
+};
+var minusOne = function minusOne() {
+  return console.log('minusOne');
+};
+var reset = function reset() {
+  return console.log('Reset');
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'reset'
+  )
+);
+
+ReactDOM.render(templateTwo, document.getElementById('root'));
--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
 console.log('App js is running 👍');
-console.log(react);
 
-var template = <p>this1 is 1JSX from app.js</p>;
-reactDOM.render(template, document.getElementById('root'));
+const app = {
+  title: 'Title',
+  subTitle: 'sub title',
+  options: ['One', 'Two', 'Three', 'Four'],
+};
+
+const isTitleExists = () => app.title;
+
+var template = (
+  <div>
+    {app.title && <h1>{app.title}</h1>}
+    {app.subTitle && <p>app.subTitle</p>}
+    {app.options && app.options.length > 0 && <p>here are your options</p>}
+    <ol>
+      {app.options.map((option, index) => (
+        <li key={index}>{option}</li>
+      ))}
+    </ol>
+  </div>
+);
+
+let count = 0;
+const addOne = () =>  console.log('AddOne');
+const minusOne = () =>  console.log('minusOne');
+const reset = () =>  console.log('Reset');
+
+
+const templateTwo = (
+  <div>
+    <h1>Count: {count}</h1>
+    <button  onClick={addOne} >
+      +1
+    </button>
+    <button  onClick={minusOne} >
+      -1
+    </button>
+    <button  onClick={reset} >
+       reset
+    </button>
+  </div>
+);
+
+ReactDOM.render(templateTwo, document.getElementById('root'));
